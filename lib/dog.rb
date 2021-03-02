1f56d7095d8168a8927a359d9a21b96f02d8ff80
Jose Soto <jsoto7087@gmail.com>
2021-03-01 22:14:12 -0500

    commit tree 6f957d2f1eb74a31665f0cc3e39d32a54d5b8980

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,41 @@
 # Add your code here
+
+class Dog 
+    
+    attr_accessor :name, :clear_all, :print_all
+   
+    @@all = []
+    # @@save = []
+
+    def initialize(name)
+        self.name = name
+       
+        self.save
+        # @@save << save
+
+    end 
+
+    def self.all
+        @@all
+    end 
+    
+    def self.clear_all
+       @@all.clear
+
+    end 
+
+    def self.print_all
+         @@all.each do |n|
+            puts n.name
+         end 
+       
+    end 
+
+    def save
+      
+        @@all << self
+    end 
+   
+
+
+end 
